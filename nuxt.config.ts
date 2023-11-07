39cf9ae06b0b28ca444100d2bd44bd2f8ed71626
ZhaoHouLin <rodes5292@gmail.com>
2023-11-07 16:26:14 +0800

    [Edit] Update nuxt.config.ts,修改網址

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     token: "",
     public: {
-      apiUrl: "https://url-shortener-nuxt.vercel.app/",
+      apiUrl: "https://zz-url.vercel.app/",
     },
   },
 
